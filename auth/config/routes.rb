9f3985430a8c55100c1b75c3ba463dba719e8115
Александр <root@rightway.by>
2021-03-14 21:26:46 +0300

    Система авторизации

--- auth/config/routes.rb
+++ auth/config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
+
+  use_doorkeeper
+  devise_for :accounts, controllers: {
+    registrations: 'accounts/registrations',
+    sessions: 'accounts/sessions'
+  }
+
+  root to: 'accounts#index'
+
+  resources :accounts, only: [:index, :edit, :update, :destroy] do
+    collection do
+      get :current
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
